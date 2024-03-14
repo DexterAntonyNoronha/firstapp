ba16454c2068674976b9ba89bee64c785e3f84ab
DexterAntonyNoronha <dexter.a.noronha@gmail.com>
2024-03-14 11:22:33 +0530

    Modified file

--- src/ReactFirstPage.jsx
+++ src/ReactFirstPage.jsx
@@ -5,7 +5,7 @@ export function FirstPage() {
     let title = "This is my first app!"; // Attributes
 
     const items = ['Home', 'About Us', 'Contact US', 'Gallery'];
-    const numbers = [1,2,3,4,5];
+    const numbers = [1, 2, 3, 4, 5];
 
     const fromChild = () => {
         console.log("This is called from Child");
@@ -22,8 +22,8 @@ export function FirstPage() {
             {/* {numbers.map((x => <h2 key={x}>{x}</h2>))} */}
 
             {/* Expressions example - wrapped using curly braces {} */}
-            <h1>First Page File - {title}</h1>
-            
+            <h1>This is First Page File - {title}</h1>
+
             {/* Here we are injecting another component to the main page */}
             <ReactListExample menuItems={items} fromChildFunction={fromChild}></ReactListExample>
             {/* <ReactListExample/> */}
